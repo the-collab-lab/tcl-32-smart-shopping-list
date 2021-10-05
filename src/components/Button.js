30afc849b1e01b6d6cc861408aa52c41e3086ce6
Jordan Raleigh <60301522+JordanRaleigh@users.noreply.github.com>
2021-10-04 20:15:12 -0500

    Added Firebase connection and write functionality through a button

---
+++ src/components/Button.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { collection, addDoc } from 'firebase/firestore';
+import { db } from '../lib/firebase';
+
+function Button() {
+  const [state, setState] = useState(0);
+
+  const handleSubmission = async () => {
+    setState(state + 1);
+    const docRef = await addDoc(collection(db, 'users'), {
+      state: state,
+    });
+    console.log('Document written with ID: ', docRef.id);
+  };
+
+  return (
+    <div>
+      <button onClick={handleSubmission}>Add item</button>
+    </div>
+  );
+}
+
+export default Button;
