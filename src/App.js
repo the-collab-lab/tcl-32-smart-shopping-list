2aa07e22b88ec0f03ebc35a19a0c394b1c707297
Fatima Camero <fcamero12@gmail.com>
2021-10-05 19:51:55 -0700

    Add styling to Links
    Co-authored-by: shalom <shalomseleshi@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,11 +3,27 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
+  const linkContainerStyle = {
+    position: 'fixed',
+    bottom: '0',
+    width: '100%',
+  };
+
+  const linkStyle = {
+    padding: '1rem',
+  };
+
   return (
     <div className="App">
       <Router>
-        <Link to="/list">List</Link>
-        <Link to="/additem">Add Item</Link>
+        <div style={linkContainerStyle}>
+          <Link style={linkStyle} to="/list">
+            List
+          </Link>
+          <Link style={linkStyle} to="/additem">
+            Add Item
+          </Link>
+        </div>
       </Router>
     </div>
   );
