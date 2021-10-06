5c9bc952f91d2d7235d360d0b56b8bd545fed1c0
Jordan Raleigh <60301522+JordanRaleigh@users.noreply.github.com>
2021-10-05 21:27:24 -0500

    added logic to read from firestore on Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,21 +1,50 @@
-import React, { useState } from 'react';
-import { collection, addDoc } from 'firebase/firestore';
+import React, { useState, useEffect } from 'react';
+import { collection, addDoc, getDocs } from 'firebase/firestore';
 import { db } from '../lib/firebase';
 
 function Button() {
   const [state, setState] = useState(0);
+  const [user, setUser] = useState([]);
 
   const handleSubmission = async () => {
     setState(state + 1);
     const docRef = await addDoc(collection(db, 'users'), {
       state: state,
     });
-    console.log('Document written with ID: ', docRef.id);
+    console.log('Button was clicked =>', state);
   };
-  // test
+
+  const fetchUsers = async () => {
+    //console.log(db);
+    const response = await getDocs(collection(db, 'users'));
+
+    console.log(response);
+    //const data = await response.get();
+    response.docs.forEach((item) => {
+      setUser([...user, item.data()]);
+    });
+    // console.log(
+    //   response.docs.forEach((item) => {
+    //     setUsers([...users, item.data()]);
+    //   }),
+    // );
+  };
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
     <div>
       <button onClick={handleSubmission}>Add item</button>
+      {user &&
+        user.map((user) => {
+          return (
+            <div className="user-container">
+              <h4>{user.title}</h4>
+              <p>{user.body}</p>
+            </div>
+          );
+        })}
     </div>
   );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -6,15 +6,9 @@ export default function List() {
   const showList = async () => {
     const querySnapshot = await getDocs(collection(db, 'users'));
     querySnapshot.forEach((doc) => {
-      console.log(`${doc.id} => ${doc.data().state}`);
+      return doc.data().state;
     });
   };
 
-  //const doc =
-  // Collection:
-  // {doc.map((doc, index) => (
-  //   <p key={index}>{JSON.stringify(doc.data().state)}</p>
-  // ))}
-
-  return <div>Hi</div>;
+  return <div>{showList().state}</div>;
 }
