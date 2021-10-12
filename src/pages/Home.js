86ab51fd8dc60b7d961640b3e5ae16e89d423633
Fatima Camero <fcamero12@gmail.com>
2021-10-12 10:04:32 -0700

    check if token is in localStorage and save

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CreateListButton from '../components/CreateListButton';
 
 function Home() {
+  const [userToken, setUserToken] = useState(null);
+
+  useEffect(() => {
+    const userTokenJSON = window.localStorage.getItem('userToken');
+
+    if (userTokenJSON) {
+      setUserToken(JSON.parse(userTokenJSON));
+      console.log('token found');
+    } else {
+      console.log('token not found');
+    }
+  }, [userToken]);
+
+  console.log('userToken', userToken);
+
   return (
     <div>
       <CreateListButton />
