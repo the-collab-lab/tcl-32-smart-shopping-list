d964f36620fe9078a3dcd127ddb3fef1119eaafa
Fatima Camero <fcamero12@gmail.com>
2021-10-23 18:02:25 -0700

    test code to compare userToken in database with user input

--- src/components/TokenForm.js
+++ src/components/TokenForm.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { db } from '../lib/firebase';
+// import { collection, where } from "firebase/firestore";
 
 export default function TokenForm({ createTokenAndSaveToLocalStorage }) {
-  const handleTokenSubmit = (e) => {
+  const handleTokenSubmit = async (e) => {
     e.preventDefault();
+    // const token = e.target.sharedToken.value
+
+    db.collection('list')
+      .where('userToken', '==', 'sharedToken')
+      .get()
+      .then((querySnapshot) => {
+        console.log('querySnapshot', querySnapshot);
+        // querySnapshot.forEach(doc =>{
+        //     console.log(doc.data()) // or do something else with the data
+        // })
+        createTokenAndSaveToLocalStorage(e.target.sharedToken.value);
+      })
+      .catch((error) => {
+        alert('Error: This token does not exist', error);
+      });
+
     console.log(e.target.sharedToken.value);
-    createTokenAndSaveToLocalStorage(e.target.sharedToken.value);
+    // createTokenAndSaveToLocalStorage(e.target.sharedToken.value);
   };
 
   return (
