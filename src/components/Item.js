cc841813855251ba707d27ae096936439944958d
Fatima Camero <fcamero12@gmail.com>
2021-11-02 21:18:34 -0700

    get lastPurchased date
    Co-authored-by: Mandi Hamza <mandihamza@gmail.com>
    Co-authored-by: Fatima Camero <fcamero12@gmail.com>

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../components/AddItemForm.css';
 import { updateDoc, serverTimestamp, doc } from 'firebase/firestore';
 import { useState } from 'react/cjs/react.development';
@@ -6,7 +6,13 @@ import { db } from '../lib/firebase';
 
 function Item({ item, i }) {
   const [checked, setChecked] = useState(false);
+  const [durationSincePurchased, setDurationSincePurchased] = useState(null);
   const userToken = window.localStorage.getItem('userToken');
+
+  const lastPurchased = item.lastPurchased.toDate();
+  console.log('lastPurchased', lastPurchased);
+
+  console.log(`24hours = ${Math.floor(item.lastPurchased / 86400)}`);
 
   const updateLastPurchased = async (item) => {
     console.log(item.id);
@@ -30,11 +36,11 @@ function Item({ item, i }) {
               onClick={() => updateLastPurchased(item)}
             />
             Purchased
-            <input
+            {/* <input
               value={console.log('Null: ', null)}
               name="itemPurchased"
               type="hidden"
-            />
+            /> */}
           </label>
         </form>
       </div>
