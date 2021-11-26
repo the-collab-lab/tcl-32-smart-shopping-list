a148f75055f9af5c231891ef41e96ede5f877468
Jordan Raleigh <60301522+JordanRaleigh@users.noreply.github.com>
2021-11-26 17:57:56 -0600

    afilter style added

--- src/components/SearchList.js
+++ src/components/SearchList.js
@@ -28,32 +28,55 @@ export default function SearchList({ listItems, userToken }) {
 
   return (
     <div>
-      Filter Items:
-      <input
-        type="text"
-        ref={searchInputRef}
-        placeholder="Search for Items here"
-        aria-label="Search for Items here"
-        onChange={updateSearch}
-      />
-      {searchedItem && (
-        <button aria-label="clear search field" onClick={clearSearch}>
-          X
-        </button>
-      )}
-      <div className="container flex flex-col mx-auto w-full items-center justify-center">
-        <ul className="items-list flex flex-col items-center w-full">
-          {filteredList.map((item) => {
-            return (
-              <Item
-                key={item.id}
-                item={item}
-                userToken={userToken}
-                focusOnInput={focusOnInput}
-              />
-            );
-          })}
-        </ul>
+      <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center text-lg font-bold text-gray-500 bg-white p-2 w-96 rounded-md mt-5 mb-2 border-2 py-1 px-3">
+          <input
+            type="text"
+            ref={searchInputRef}
+            placeholder="Search for Items here"
+            aria-label="Search for Items here"
+            onChange={updateSearch}
+            className="flex-grow outline-none text-gray-600 focus:text-blue-600"
+          />
+          {searchedItem ? (
+            <button aria-label="clear search field" onClick={clearSearch}>
+              X
+            </button>
+          ) : (
+            <spa>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6 text-gray-400 hover:text-blue-400 transition duration-100 cursor-pointer"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </spa>
+          )}
+        </div>
+      </div>
+      <div>
+        <div className="container flex flex-col mx-auto w-full items-center justify-center">
+          <ul className="items-list flex flex-col items-center w-full">
+            {filteredList.map((item) => {
+              return (
+                <Item
+                  key={item.id}
+                  item={item}
+                  userToken={userToken}
+                  focusOnInput={focusOnInput}
+                />
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </div>
   );
