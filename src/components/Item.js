ccee1c520592f3ab537ddbbcaa104b75ebd5afe5
Fatima Camero <fcamero12@gmail.com>
2021-11-26 21:56:17 -0800

    update item style and checkbox style
    Co-authored-by: Mandi Hamza <mandihamza@gmail.com>
    Co-authored-by: Shalom Kebede <shalomseleshi@gmail.com>
    Co-authored-by: Jordan Raleigh <jordankeithraleigh@gmail.com>
    Co-authored-by: Fatima Camero <fcamero12@gmail.com>

--- src/components/Item.js
+++ src/components/Item.js
@@ -64,34 +64,45 @@ function Item({ item, userToken, focusOnInput }) {
   };
 
   let itemClass = '';
+  let checkboxClass = '';
   let nameAriaLabel = '';
 
   switch (true) {
     case !isActive(item, daysSincePurchased):
-      itemClass = 'bg-gray-300';
+      itemClass = 'border-solid border-2 border-gray-300';
+      checkboxClass =
+        'border-gray-300 text-gray-300 focus:border-gray-300 focus:ring-gray-300';
       nameAriaLabel = `${item.itemName}. is inactive.`;
       break;
     case item.daysUntilNextPurchase >= 2 && item.daysUntilNextPurchase <= 7:
-      itemClass = 'bg-strong-lime-green';
+      itemClass = 'border-solid border-2 border-strong-lime-green';
+      checkboxClass =
+        'border-strong-lime-green text-strong-lime-green focus:border-strong-lime-green focus:ring-strong-lime-green';
       nameAriaLabel = `${item.itemName}. estimated purchase time is soon`;
       break;
     case item.daysUntilNextPurchase >= 8 && item.daysUntilNextPurchase <= 30:
-      itemClass = 'bg-maroon-flush';
+      itemClass = 'border-solid border-2 border-maroon-flush';
+      checkboxClass =
+        'border-maroon-flush text-maroon-flush focus:border-maroon-flush focus:ring-maroon-flush';
       nameAriaLabel = `${item.itemName}. estimated purchase time is kind of soon.`;
       break;
     case item.daysUntilNextPurchase > 30:
-      itemClass = 'bg-red-damask';
+      itemClass = 'border-solid border-2 border-red-damask';
+      checkboxClass =
+        'border-red-damask text-red-damask focus:border-red-damask focus:ring-red-damask';
       nameAriaLabel = `${item.itemName}. estimated purchase time is not soon.`;
       break;
     default:
-      itemClass = 'bg-gray-300';
+      itemClass = 'border-solid border-2 border-gray-300';
+      checkboxClass =
+        'border-gray-300 text-gray-300 focus:border-gray-300 focus:ring-gray-300';
       nameAriaLabel = `${item.itemName}. is inactive.`;
   }
 
   return (
     <li className="item border-gray-400 flex flex-row mb-2 w-9/12 md:w-2/4">
       <div
-        className={`${itemClass} shadow select-none dark:bg-gray-800 rounded-md flex flex-1 items-center p-4 bg-opacity-60`}
+        className={`${itemClass} shadow select-none dark:bg-gray-800 rounded-md flex flex-1 items-center p-4 bg-white bg-opacity-90`}
       >
         <div className="flex flex-col w-5 h-5 justify-center items-center mr-4">
           <form>
@@ -102,6 +113,7 @@ function Item({ item, userToken, focusOnInput }) {
               checked={checked}
               name="itemPurchased"
               onChange={handleCheckboxChange}
+              className={`${checkboxClass} appearance-none form-checkbox bg-white h-6 w-6 border rounded-md checked:border-transparent focus:ring focus:ring-offset-1 `}
             />
           </form>
         </div>
