6e50d056c1bf71375fc442fda0b6f7c6a301b532
mandihamza <mandihamza@gmail.com>
2021-11-27 18:39:47 -0700

    update modal styles to improve centering on large screens

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -13,16 +13,13 @@ export default function Modal({ open, children }) {
         role="dialog"
         aria-modal="true"
       >
-        <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center p-0">
           <div
             className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
             aria-hidden="true"
           ></div>
 
-          <span
-            className="hidden sm:inline-block sm:align-middle sm:h-screen"
-            aria-hidden="true"
-          >
+          <span className="hidden align-middle h-screen" aria-hidden="true">
             &#8203;
           </span>
           <div>{children}</div>
