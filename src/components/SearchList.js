9a8ab9978cade5b1117118e26276a83685425a67
mandihamza <mandihamza@gmail.com>
2021-11-27 18:54:00 -0700

    minor syntax fixes

--- src/components/SearchList.js
+++ src/components/SearchList.js
@@ -43,22 +43,22 @@ export default function SearchList({ listItems, userToken }) {
               X
             </button>
           ) : (
-            <spa>
+            <span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6 text-gray-400 hover:text-blue-400 transition duration-100 cursor-pointer"
+                className="h-6 w-6 text-gray-400 hover:text-blue-400 transition duration-100 cursor-pointer"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
                 />
               </svg>
-            </spa>
+            </span>
           )}
         </div>
       </div>
