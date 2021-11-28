0c1bfb5fb45ecb3a8324001abc445a3c9027ef4e
Jordan Raleigh <60301522+JordanRaleigh@users.noreply.github.com>
2021-11-28 09:31:18 -0600

    added styling to addItemForm

--- src/components/AddItemForm.js
+++ src/components/AddItemForm.js
@@ -66,46 +66,64 @@ function AddItemForm() {
   };
 
   return (
-    <form onSubmit={submitItem} className="addItemForm">
-      <label htmlFor="item">
-        Item Name
-        <input id="itemName" type="text" />
-      </label>
-      <div className="nextPurchaseRadioGroup">
-        <label>
+    <form onSubmit={submitItem}>
+      <div className="flex justify-center items-center">
+        <label
+          htmlFor="item"
+          className="flex justify-center items-center text-lg font-bold text-gray-500 bg-white p-2 w-96 rounded-md mt-5 mb-2 border-2 py-1 px-3"
+        >
+          Item Name
+          <input
+            id="itemName"
+            type="text"
+            className="flex-grow outline-none text-gray-600 focus:text-blue-600"
+          />
+        </label>
+      </div>
+      <div className="item border-gray-400 flex flex-row mb-2">
+        <label className="border-solid border-2 border-strong-lime-green shadow select-none dark:bg-gray-800 rounded-md flex flex-1 items-center p-4 bg-white bg-opacity-90 flex justify-center items-center">
           <input
             id="soon"
             type="radio"
             value="7"
             name="nextPurchase"
             required
+            className="form-radio h-5 w-5"
           />
           Soon
         </label>
-        <label>
+        <label className="border-solid border-2 border-maroon-flush shadow select-none dark:bg-gray-800 rounded-md flex flex-1 items-center p-4 bg-white bg-opacity-90 flex justify-center items-center">
           <input
             id="kindOfSoon"
             type="radio"
             value="14"
             name="nextPurchase"
             required
+            className="form-radio h-5 w-5"
           />
           Kind of Soon
         </label>
-        <label>
+        <label className="border-solid border-2 border-red-damask shadow select-none dark:bg-gray-800 rounded-md flex flex-1 items-center p-4 bg-white bg-opacity-90 flex justify-center items-center">
           <input
             id="notSoon"
             type="radio"
             value="30"
             name="nextPurchase"
             required
+            className="form-radio h-5 w-5"
           />
           Not Soon
         </label>
       </div>
-      <button id="addItem" type="submit" className="addItemSubmitButton">
-        Add To List
-      </button>
+      <div>
+        <button
+          id="addItem"
+          type="submit"
+          className="bg-red-damask font-semibold text-white font-serif font-bold p-2 w-1/2 rounded-full hover:bg-maroon-flush focus:outline-none focus:ring shadow-lg hover:shadow-none transition-all duration-300 m-2"
+        >
+          Add To List
+        </button>
+      </div>
     </form>
   );
 }
